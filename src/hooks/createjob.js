b4a47c744f56f7f57ed9679d2e83cc0e7b57e6a0
Nandha <nandhakumar1411@gmail.com>
2025-02-26 12:06:49 +0530

    Create Role

--- src/hooks/createjob.js
+++ src/hooks/createjob.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export function createJob(_data){
-    return axios.post('https://wroots-backend.onrender.com/createJob',_data);
+    return axios.post('https://api.wraeglobal.com/createJob',_data);
 }
